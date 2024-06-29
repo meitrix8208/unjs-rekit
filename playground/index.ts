7c76d71ede43b76d01dc054c993bdfef8d1f2d41
meitrix8208 <maurrod2001@outlook.com>
2024-06-29 14:24:50 -0500

    feat: Add new functions

--- playground/index.ts
+++ playground/index.ts
@@ -1,4 +1,76 @@
-import { destr } from "../dist/object";
+import { destr, defu } from "../dist/object";
+import { hash, isEqual, randomUUID } from "../dist/crypto";
+import { runtimeInfo, isWindows, platform } from "../dist/env"
 import { consola } from "../dist/cli";
+import { ofetch } from "../dist/fetch";
+import { join, parse } from "../dist/path";
+import { withBase, withQuery } from "../dist/url";
+import { char, createRegExp, digit, maybe, oneOrMore } from "../dist/regex";
+import { kebabCase } from "../dist/string";
+import { createApp, eventHandler, toWebHandler } from "../dist/https";
 
-consola.info(destr('{ "deno": "yay" }'));
+let runtime = destr<{ deno: string, node: string, bun: string }>('{ "deno": "perl" }')
+const other = { deno: "NO-RUST", node: "c++", bun: "zig" }
+runtime = defu(runtime, other)
+runtime.deno = "rust"
+consola.info(runtime.deno, runtime.node, runtime.bun);
+
+const hash1 = hash(runtime.bun)
+const hash2 = hash(runtime.bun)
+const hash3 = hash(other.bun)
+
+consola.info(hash1, hash2, hash3)
+consola.info(isEqual(hash1, hash2))
+consola.info(isEqual(hash1, hash3))
+
+consola.info(platform)
+consola.info(isWindows)
+consola.info(runtimeInfo)
+
+try {
+  const res = await ofetch("https://api.github.com/repos/octocat/Hello-World");
+  consola.info(res.description);
+} catch (error) {
+  consola.error(error);
+}
+
+const path = join("a", "b", "c")
+const parsed = parse(path)
+consola.info(path, parsed)
+
+const base = "https://example.com"
+const url = withBase("/foo", base)
+const url2 = withQuery(url, { bar: "baz" })
+consola.info(url, url2)
+
+const string = "FooBar"
+const kebab = kebabCase(string)
+consola.info(kebab)
+
+const regex = createRegExp(
+  oneOrMore(digit).groupedAs('major'),
+  '.',
+  oneOrMore(digit).groupedAs('minor'),
+  maybe('.', oneOrMore(char).groupedAs('patch'))
+)
+
+const match = regex.exec("1.2.3")
+consola.info(match?.groups)
+
+const uuid = randomUUID()
+
+consola.info(uuid)
+
+const app = createApp()
+app.use("/", eventHandler((event) => {
+  console.log(event.method, event.node.req.url, event.node.req.headers);
+  return new Response("Hello, World!", { status: 200 })
+}))
+
+const handler = toWebHandler(app);
+const response = await handler(new Request(
+  new URL("/", "http://localhost"), { headers: { "x-test": "test" } }
+));
+const text = await response.text();
+consola.info(text);
+
